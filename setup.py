3a636482db05d82730fa46198e3fe55b0511766a
Sam Larsen <sam@slarsen.io>
2023-02-26 06:24:07 -0500

    1.0.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="gptpy",
+    version="1.0.0",
+    author="Sam Larsen",
+    license="GPL-3.0-only",
+    python_requires=">=3.9",
+    install_requires=["openai", "python-dotenv"]
+)
