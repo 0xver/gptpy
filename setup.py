12a673de9845b2249769ca6850cf743837da134b
Sam Larsen <sam@slarsen.io>
2023-03-11 11:36:28 -0500

    1.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="gptpy",
-    version="1.0.1",
+    version="1.0.2",
     author="Sam Larsen",
     license="GPL-3.0-only",
     python_requires=">=3.9",
-    install_requires=["openai", "python-dotenv"]
+    install_requires=[requirements],
+    zip_safe=False
 )
