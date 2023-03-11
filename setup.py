ae901731191ea632ae281fae8133a7cc2a7eb108
Sam Larsen <sam@slarsen.io>
2023-03-11 12:19:42 -0500

    1.0.3

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ with open("requirements.txt") as f:
 
 setup(
     name="gptpy",
-    version="1.0.2",
+    version="1.0.3",
     author="Sam Larsen",
     license="GPL-3.0-only",
-    python_requires=">=3.9",
-    install_requires=[requirements],
-    zip_safe=False
+    python_requires=">=3.8",
+    install_requires=[requirements]
 )
